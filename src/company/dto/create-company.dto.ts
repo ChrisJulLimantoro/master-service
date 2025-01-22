a3ec94a97267ace7e5b76780af24b34fe8522293
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2025-01-22 21:09:35 +0700

    feat: add description to company

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -3,11 +3,18 @@ export class CreateCompanyRequest {
   code: string;
   name: string;
   owner_id: string;
+  description: string | null;
 
-  constructor(data: { code: string; name: string; owner_id: string }) {
+  constructor(data: {
+    code: string;
+    name: string;
+    owner_id: string;
+    description: string | null;
+  }) {
     this.code = data.code;
     this.name = data.name;
     this.owner_id = data.owner_id;
+    this.description = data.description;
   }
 
   static schema() {
@@ -15,6 +22,7 @@ export class CreateCompanyRequest {
       code: z.string().max(5),
       name: z.string().min(5).max(255),
       owner_id: z.string().uuid(),
+      description: z.string().max(255).nullable().optional(),
     });
   }
 }
--- src/company/dto/update-company.dto.ts
+++ src/company/dto/update-company.dto.ts
@@ -2,16 +2,23 @@ import { z } from 'zod';
 export class UpdateCompanyRequest {
   code: string | null;
   name: string | null;
+  description: string | null;
 
-  constructor(data: { code: string | null; name: string | null }) {
+  constructor(data: {
+    code: string | null;
+    name: string | null;
+    description: string | null;
+  }) {
     this.code = data.code;
     this.name = data.name;
+    this.description = data.description;
   }
 
   static schema() {
     return z.object({
       code: z.string().max(5).optional(),
       name: z.string().min(5).max(255).optional(),
+      description: z.string().max(255).nullable().optional(),
     });
   }
 }
