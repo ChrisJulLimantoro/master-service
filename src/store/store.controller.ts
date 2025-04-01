db89674c196d583a055a5f171ab3bfb2709bfa4d
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2025-04-01 22:25:31 +0700

    fix: filter store based on owner id

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -33,7 +33,13 @@ export class StoreController {
     ],
   })
   async findAll(@Payload() data: any): Promise<CustomResponse> {
-    const filter = { company_id: data.body.company_id };
+    var filter = {  };
+    const filterdata = data.body;
+    if (filterdata.company_id) {
+      filter['company'] = { id: filterdata.company_id };
+    } else {
+      filter['company'] = { owner_id: filterdata.owner_id };
+    }
     return this.service.findAll(filter);
   }
 
