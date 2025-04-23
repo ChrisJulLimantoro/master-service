d0b708e8becb09f7d007d4f2aaa35718d71ce675
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2025-04-23 13:06:48 +0700

    feat: change the logic of prisma

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -2,17 +2,11 @@ import { Module } from '@nestjs/common';
 import { EmployeeController } from './employee.controller';
 import { EmployeeService } from './employee.service';
 import { EmployeeRepository } from 'src/repositories/employee.repository';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { OwnerRepository } from 'src/repositories/owner.repository';
 
 @Module({
   imports: [],
   controllers: [EmployeeController],
-  providers: [
-    EmployeeService,
-    EmployeeRepository,
-    PrismaService,
-    OwnerRepository,
-  ],
+  providers: [EmployeeService, EmployeeRepository, OwnerRepository],
 })
 export class EmployeeModule {}
--- src/owner/owner.module.ts
+++ src/owner/owner.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { OwnerController } from './owner.controller';
 import { OwnerService } from './owner.service';
 import { OwnerRepository } from 'src/repositories/owner.repository';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [OwnerController],
-  providers: [OwnerService, OwnerRepository, PrismaService],
+  providers: [OwnerService, OwnerRepository],
 })
 export class OwnerModule {}
